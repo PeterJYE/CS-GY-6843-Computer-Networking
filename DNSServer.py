411059530c4d8ac3ee690ac10cbdd844ec871e0f
Peter Ye <148128006+PeterJYE@users.noreply.github.com>
2025-07-13 00:51:14 +0800

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -43,8 +43,8 @@ def decrypt_with_aes(encrypted_data, password, salt):
     decrypted_data = f.decrypt(encrypted_data) #call the Fernet decrypt method
     return decrypted_data.decode('utf-8')
 
-salt = b'Tandon' # Remember it should be a byte-object
-password = 'your_netid@nyu.edu'  # Replace with your NYU email
+salt = b'Tandon' 
+password = 'jy3991@nyu.edu'  
 input_string = 'AlwaysWatching'
 
 encrypted_value = encrypt_with_aes(input_string, password, salt) # exfil function
